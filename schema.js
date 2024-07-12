bc7fe1510e91ac9d2bcb16de85cc65ce05405731
Ben Phillips <pben012@gmail.com>
2024-07-12 13:45:37 +1000

    added findAccident query, edited addAccident schema so it accepts an array of IDs for insurances and witnesses

--- schema.js
+++ schema.js
@@ -47,19 +47,6 @@ const typeDefs = `
         url: String!
     }
 
-    input VehicleInput {
-        registrationNumber: String!
-        make: String!
-        model: String!
-    }
-
-    input WitnessInput {
-        firstName: String!
-        lastName: String!
-        phoneNumber: String!
-        involvement: String!
-    }
-
     type accident {
         id: ID!
         user: [User!]!
@@ -90,6 +77,9 @@ const typeDefs = `
 
     type Query {
         me: User
+        findAccident(
+            accidentID: ID!
+        ): accident
     }
 
     type Mutation {
@@ -144,49 +134,10 @@ const typeDefs = `
         weatherConditions: String!
         crashDescription: String!
         photos: [PhotoInput!]
-        otherVehicles: [VehicleInput!]
-        witnesses: [WitnessInput!]
+        insurances: [ID!]
+        witnesses: [ID!]
     ): accident
-    editAccident(
-        accidentID: String!
-        date: DateTime
-        time: String
-        location: String
-        speed: String
-        weatherConditions: String
-        crashDescription: String
-        photos: [PhotoInput]
-        otherVehicles: [VehicleInput]
-        witnesses: [WitnessInput]
-    ): accident
-    deleteAccident(
-        accidentID: String!
-    ): accident
-    addPhoto(
-        accidentID: String!
-        photoURL: String!
-    ): accident
-    deletePhoto(
-        accidentID: String!
-        photoURL: String!
-    ): accident
-    addWitness(
-        accidentID: String!
-        input: WitnessInput
-    ): accident
-    deleteWitness(
-        accidentID: String!
-        phoneNumber: String!
-    ): accident
-    addOtherVehicle(
-        accidentID: String!
-        input: VehicleInput
-    ): accident
-    deleteOtherVehicle(
-        accidentID: String!
-        registrationNumber: String!
-    ): accident
-    }
+}
 `
 
 module.exports = typeDefs
